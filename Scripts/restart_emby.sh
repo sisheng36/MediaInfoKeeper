c92c63efac0e03a7d23491ddd896b0a5e3d6b783
honue <zouhy2001@gmail.com>
2026-04-28 15:28:43 +0800

    fix

--- Scripts/restart_emby.sh
+++ Scripts/restart_emby.sh
@@ -6,12 +6,17 @@ project_dir="$(cd "$script_dir/.." && pwd)"
 
 remote="${REMOTE_SSH:-root@192.168.33.100}"
 configuration="${CONFIGURATION:-Release}"
-local_dll="${LOCAL_DLL:-$project_dir/Build/bin/$configuration/net8.0/MediaInfoKeeper.dll}"
+target_framework="${TARGET_FRAMEWORK:-net8.0}"
+tokenizer_bundle="${TOKENIZER_BUNDLE:-single-rid}"
+tokenizer_rid="${TOKENIZER_RID:-linux-x64}"
+local_dll="${LOCAL_DLL:-$project_dir/Build/bin/$configuration/$target_framework/MediaInfoKeeper.dll}"
 remote_dir="${REMOTE_DIR:-/opt/emby/config/plugins}"
 compose_dir="${COMPOSE_DIR:-/opt/emby}"
 
 # 0) Build
-dotnet build "$project_dir/MediaInfoKeeper.csproj" -c "$configuration"
+dotnet build "$project_dir/MediaInfoKeeper.csproj" -c "$configuration" -f "$target_framework" \
+  /p:TokenizerBundle="$tokenizer_bundle" \
+  /p:TokenizerRid="$tokenizer_rid"
 
 # 1) Copy DLL
 scp "$local_dll" "${remote}:${remote_dir}/"
